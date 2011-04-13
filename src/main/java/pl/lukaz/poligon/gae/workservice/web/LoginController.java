8ca2785a28e88c45757b5d16a71b3dc5fdf448b8
unknown <avasciak20@hotmail.com>
2011-04-13 13:50:35 +0200

    removed unused login

--- src/main/java/pl/lukaz/poligon/gae/workservice/web/LoginController.java
+++ src/main/java/pl/lukaz/poligon/gae/workservice/web/LoginController.java
@@ -1,7 +1,5 @@
 package pl.lukaz.poligon.gae.workservice.web;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-	
-	private Log log=LogFactory.getLog(LoginController.class);
 	
 	@RequestMapping(value="hello")
 	public void hello(Model model){
